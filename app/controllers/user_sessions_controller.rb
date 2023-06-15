c85dde4a2fb4bebfa862d93f41b8d6b6f02a18ef
ken0727 <ayame0727@gmail.com>
2023-06-15 17:59:55 +0900

    フラッシュ追加（翻訳中）

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,14 +8,17 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
+      flash[:success] = t(".success")
       redirect_back_or_to root_path
     else
+      flash.now[:danger] = t('.fail')
       render :new
     end
   end
 
   def destroy
     logout
+    flash[:danger] = t(".logout")
     redirect_to root_path
   end
 end
