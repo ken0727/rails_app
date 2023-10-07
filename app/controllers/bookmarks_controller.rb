e61a6161a676c67009db0b82abfecb598d68afbf
ken0727 <ayame0727@gmail.com>
2023-10-08 07:51:32 +0900

    cp15完了

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,10 @@
 class BookmarksController < ApplicationController
 
+  def index 
+    @boards = Board.all
+    @bookmarked_boards = current_user.bookmarked_boards
+  end
+
   def create
     board = Board.find(params[:board_id])
     bookmark = board.bookmarks.build(user_id: current_user.id)
@@ -13,11 +18,20 @@ class BookmarksController < ApplicationController
 def destroy
   # ユーザーが現在のボードに関連付けたブックマークを見つけます
   bookmark = current_user.bookmarks.find_by(board_id: params[:board_id])
+  
   if bookmark.present?
     bookmark.destroy
   end
-
-  redirect_to boards_path
+  
+  # リダイレクト先を条件に応じて設定
+  if request.referer.include?("bookmarks")
+    # ブックマーク一覧ページから来た場合
+    redirect_to bookmarks_boards_path
+  else
+    # それ以外の場合（掲示板一覧ページから来た場合を含む）
+    redirect_to boards_path
+  end
 end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ root 'static_pages#top'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  get '/bookmarks', to: 'bookmarks#index', as: 'bookmarks'
+  get '/bookmarks', to: 'bookmarks#index',  as: 'bookmarks_boards'
 
   resources :boards, only: %i[new index create edit]
   resources :users, only: %i[new create]
